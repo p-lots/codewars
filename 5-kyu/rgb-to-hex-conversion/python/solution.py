c552f21a23bd1088041bcc573ef451f3e56baced
Paul Calotta <paul.calotta@gmail.com>
2019-07-06 18:25:26 -0400

    Fixed Python solution

--- 5-kyu/rgb-to-hex-conversion/python/solution.py
+++ 5-kyu/rgb-to-hex-conversion/python/solution.py
@@ -1,9 +1,6 @@
-func rgb(_ r: Int, _ g: Int, _ b: Int) -> String {
-    return String(format: "%02X%02X%02X", clamp(r), clamp(g), clamp(b))
-}
+def clamp(n):
+    return min(max(n, 0), 255)
 
-func clamp(_ n: Int) -> Int {
-    if n > 255 { return 255 }
-    else if n < 0 { return 0 }
-    else { return n }
-}
+def rgb(r, g, b):
+    r, g, b = clamp(r), clamp(g), clamp(b)
+    return '{0:02x}{1:02x}{2:02x}'.format(r, g, b).upper()
