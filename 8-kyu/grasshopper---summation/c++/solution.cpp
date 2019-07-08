c464aadcc2df0d307300fb07fc559c43f9b551c9
Paul Calotta <paul.calotta@gmail.com>
2019-07-07 23:58:53 -0400

    Fixed C++ solution

--- 8-kyu/grasshopper---summation/c++/solution.cpp
+++ 8-kyu/grasshopper---summation/c++/solution.cpp
@@ -1,9 +1,4 @@
-#include <algorithm>
-#include <numeric>
-#include <vector>
-
-int summation(int num){
-    std::vector<int> nums(num);
-    std::iota(nums.begin(), nums.end(), 1);
-    return std::accumulate(nums.begin(), nums.end(), 0);
+int summation(int num)
+{
+    return (num * (num + 1)) / 2;
 }
