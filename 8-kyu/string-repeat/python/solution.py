1eb36a1957519a3761854194b3eeabd9ee7a25b1
Paul Calotta <paul.calotta@gmail.com>
2019-08-21 23:28:24 -0400

    Fixed Python solution for 8-kyu/string-repeat

--- 8-kyu/string-repeat/python/solution.py
+++ 8-kyu/string-repeat/python/solution.py
@@ -1,10 +1,2 @@
-func repeatStr(_ n: Int, _ string: String) -> String {
-    if n < 1 {
-        return ""
-    }
-    var ret : String = string
-    for _ in 1..<n {
-        ret += string
-    }
-    return ret
-}
+def repeat_str(repeat, strng):
+    return strng * repeat
