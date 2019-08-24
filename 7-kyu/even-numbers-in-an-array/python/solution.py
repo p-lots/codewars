8ba81ca3d020348db0042ce938e525c349ec83e6
Paul Calotta <paul.calotta@gmail.com>
2019-08-23 23:08:58 -0400

    Fixed Python solution for 7-kyu/even-numbers-in-an-array

--- 7-kyu/even-numbers-in-an-array/python/solution.py
+++ 7-kyu/even-numbers-in-an-array/python/solution.py
@@ -1,16 +1,9 @@
-#include <algorithm>
-#include <vector>
-
-std::vector<int> evenNumbers(std::vector<int> arr, int n) {
-  auto it = arr.rbegin();
-  std::vector<int> ret;
-  while (n > 0 && it != arr.rend()) {
-    if (*it % 2 == 0) {
-      ret.push_back(*it);
-      n--;
-    }
-    it++;
-  }
-  std::reverse(ret.begin(), ret.end());
-  return ret;
-}
+def even_numbers(arr, n):
+    arr_even = list(filter(lambda item: item % 2 == 0, arr))
+    ret = []
+    count = 0
+    for i in reversed(arr_even):
+        if count < n:
+            ret.append(i)
+        count += 1
+    return list(reversed(ret))
