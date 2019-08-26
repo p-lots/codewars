0d3476f8b6f0da8a63d69ce27d87c45ce1add7d6
Paul Calotta <paul.calotta@gmail.com>
2019-08-25 21:23:32 -0400

    Fixed Python solution for 7-kyu/maximum-length-difference

--- 7-kyu/maximum-length-difference/python/solution.py
+++ 7-kyu/maximum-length-difference/python/solution.py
@@ -1,26 +1,6 @@
-#include <algorithm>
-
-class MaxDiffLength
-{
-public:
-    static int mxdiflg(std::vector<std::string> &a1, std::vector<std::string> &a2);
-};
-
-bool len_cmp(const std::string &lhs, const std::string &rhs)
-{
-    return lhs.length() < rhs.length();
-}
-
-int MaxDiffLength::mxdiflg(std::vector<std::string> &a1, std::vector<std::string> &a2)
-{
-    if (a1.empty() || a2.empty()) return -1;
-    std::sort(a1.begin(), a1.end(), len_cmp);
-    std::sort(a2.begin(), a2.end(), len_cmp);
-    auto a1_max = a1.rbegin();
-    auto a1_min = a1.begin();
-    auto a2_max = a2.rbegin();
-    auto a2_min = a2.begin();
-    auto l1 = a1_max->length() - a2_min->length();
-    auto l2 = a2_max->length() - a1_min->length();
-    return l1 > l2 ? l1 : l2;
-}
+def mxdiflg(a1, a2):
+    if not a1 or not a2:
+        return -1
+    a1_sorted = sorted(a1, key=len)
+    a2_sorted = sorted(a2, key=len)
+    return max(abs(len(a1_sorted[-1]) - len(a2_sorted[0])), abs(len(a2_sorted[-1]) - len(a1_sorted[0])))
