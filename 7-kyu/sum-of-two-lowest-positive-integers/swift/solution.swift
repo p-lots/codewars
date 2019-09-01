700a2080be79f35a4c5cdfd82fff24161b9fca2b
Paul Calotta <paul.calotta@gmail.com>
2019-08-31 21:46:34 -0400

    Fixed Swift solution for 7-kyu/sum-of-two-lowest-positive-integers

--- 7-kyu/sum-of-two-lowest-positive-integers/swift/solution.swift
+++ 7-kyu/sum-of-two-lowest-positive-integers/swift/solution.swift
@@ -1,7 +1,3 @@
-#include <algorithm>
-
-long sumTwoSmallestNumbers(std::vector<int> numbers)
-{
-    std::sort(numbers.begin(), numbers.end());
-    return static_cast<long>(numbers.at(0)) + static_cast<long>(numbers.at(1));
+func sumOfTwoSmallestIntegersIn(_ array: [Int]) -> Int {
+  return array.sorted()[0] + array.sorted()[1]
 }
