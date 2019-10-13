1905741cb2143e30e05ac261141509c4f77c63bd
Paul Calotta <paul.calotta@gmail.com>
2019-10-13 03:18:19 -0400

    Fixed Swift solution

--- 8-kyu/convert-number-to-reversed-array-of-digits/swift/solution.swift
+++ 8-kyu/convert-number-to-reversed-array-of-digits/swift/solution.swift
@@ -1,11 +1,3 @@
-#include <algorithm>
-#include <string>
-
-std::vector<int> digitize(unsigned long n) 
-{        
-    std::string s = std::to_string(n);
-    std::reverse(s.begin(), s.end());
-    std::vector<int> ret;
-    for (char c : s) ret.push_back(c - '0');
-    return ret;
+func digitize(_ num:Int) -> [Int] {
+    return String(num).characters.flatMap { Int(String($0)) }.reversed()
 }
