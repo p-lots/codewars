e31ca6adbf07edab6338f5511fb561c68d249a9f
Paul Calotta <paul.calotta@gmail.com>
2019-10-14 18:08:30 -0400

    Fixed Python solution for 8-kyu/count-of-positives---sum-of-negatives

--- 8-kyu/count-of-positives---sum-of-negatives/python/solution.py
+++ 8-kyu/count-of-positives---sum-of-negatives/python/solution.py
@@ -1,19 +1,11 @@
-#include <algorithm>
-#include <numeric>
-#include <vector>
-
-std::vector<int> countPositivesSumNegatives(std::vector<int> input)
-{
-    if (input.empty()) return { };
-    return {
-        int(std::count_if(input.begin(), input.end(),
-            [](const int &n) { return n > 0; })),
-        std::accumulate(input.begin(), input.end(), 0,
-            [](const int &lhs, const int &rhs) {
-                if (lhs < 0 && rhs < 0) return lhs + rhs;
-                else if (lhs < 0 && !(rhs < 0)) return lhs;
-                else if (rhs < 0) return rhs;
-                else return 0;
-            }
-        )};
-}
+def count_positives_sum_negatives(arr):
+    if not arr:
+        return arr
+    positive_count = 0
+    negative_sum = 0
+    for n in arr:
+        if n > 0:
+            positive_count += 1
+        else:
+            negative_sum += n
+    return [positive_count, negative_sum]
