142713e0473b6f80d43f7863f7de9eb4c5fbcb2e
Paul Calotta <paul.calotta@gmail.com>
2019-12-20 19:17:41 -0500

    Added Python solution for 8-kyu/basic-subclasses---adam-and-eve

---
+++ 8-kyu/basic-subclasses---adam-and-eve/python/solution.py
@@ -0,0 +1,16 @@
+def God():
+    man = Man()
+    woman = Woman()
+    return [man, woman]
+
+class Human:
+    def __init__(self):
+        pass
+
+class Man(Human):
+    def __init__(self):
+        pass
+
+class Woman(Human):
+    def __init__(self):
+        pass
