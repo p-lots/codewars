db1ea123980ecadbd325a3559e2be89748ce05ec
Paul Calotta <paul.calotta@gmail.com>
2019-12-23 19:47:46 -0500

    Added Python solution for 8-kyu/for-ufc-fans-(total-beginners)--conor-mcgregor-vs-george-saint-pierre

---
+++ 8-kyu/for-ufc-fans-(total-beginners)--conor-mcgregor-vs-george-saint-pierre/python/solution.py
@@ -0,0 +1,7 @@
+def quote(fighter):
+    fighter = fighter.lower()
+    if fighter == 'george saint pierre':
+        return 'I am not impressed by your performance.'
+    elif fighter == 'conor mcgregor':
+        return 'I\'d like to take this chance to apologize.. To absolutely NOBODY!'
+    return None
