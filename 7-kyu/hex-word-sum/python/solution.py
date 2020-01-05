2848b39c229902c1d4fe22b5ca820e523afc910c
Paul Calotta <paul.calotta@gmail.com>
2020-01-05 05:59:20 -0500

    Updated Python solution for 7-kyu/hex-word-sum

--- 7-kyu/hex-word-sum/python/solution.py
+++ 7-kyu/hex-word-sum/python/solution.py
@@ -1,16 +1,3 @@
-def is_valid_string(words):
-    for letter in words:
-        if letter in 'GHIJKLMNPQRTUVWXYZ':
-            return False
-    return True
-
-def hex_word_sum(s):
-    if not s:
-        return 0
-    ret = 0
-    s = s.replace('S', '5').replace('O', '0')
-    for word in s.split():
-        if not is_valid_string(word):
-            continue
-        ret += int(word, 16)
-    return ret
+def hex_word_sum(strng):
+    strng = strng.replace('S', '5').replace('O', '0')
+    return sum(int(word, 16) if all(letter in '0123456789ABCDEF' for letter in word) else 0 for word in strng.split())
