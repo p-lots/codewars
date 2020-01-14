ca5b21901556c94fd5ba897b7ad461e1f1e97d3b
Paul Calotta <paul.calotta@gmail.com>
2020-01-14 03:20:30 -0500

    Updated Swift solution for 7-kyu/moves-in-squared-strings-(i)

--- 7-kyu/moves-in-squared-strings-(i)/swift/solution.swift
+++ 7-kyu/moves-in-squared-strings-(i)/swift/solution.swift
@@ -1,10 +1,11 @@
 func horMirror(_ s: String) -> String {
     return s.components(separatedBy: "\n").reversed().joined(separator: "\n")
 }
+
 func vertMirror(_ s: String) -> String {
-    return s.components(separatedBy: "\n").map { String($0.characters.reversed()) }.joined(separator: "\n")
+    return s.components(separatedBy: "\n").map { String($0.reversed()) }.joined(separator: "\n")
 }
-// replace the dots with function parameter
+
 func oper(_ f: (String) -> String, _ s: String) -> String {
     return f(s)
-} 
+}
