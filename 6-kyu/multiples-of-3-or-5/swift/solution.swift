f39fcbc44e8a7166c394f578dea182db389a30c3
Paul Calotta <paul.calotta@gmail.com>
2020-02-18 07:42:11 -0500

    Fix Swift solution for 6-kyu/multiples-of-3-or-5

--- 6-kyu/multiples-of-3-or-5/swift/solution.swift
+++ 6-kyu/multiples-of-3-or-5/swift/solution.swift
@@ -1,20 +1,4 @@
-int solution(int number) 
-{
-    if (number <= 3) return 0;
-    else if (number == 4 || number == 5) return 3;
-    int ret = 0;
-    bool arr[number];
-    for (unsigned i = 0; i < number; i++) {
-        arr[i] = false;
-    }
-    for (unsigned i = 3; i < number; i += 3) {
-        arr[i] = true;
-    }
-    for (unsigned i = 5; i < number; i += 5) {
-        arr[i] = true;
-    }
-    for (unsigned i = 3; i < number; i++) {
-        if (arr[i]) ret += i;
-    }
-    return ret;
+func solution(_ num: Int) -> Int {
+  guard num > 3 else { return num == 3 ? 3 : 0 }
+  return (3..<num).filter { $0 % 5 == 0 || $0 % 3 == 0 }.reduce(0, +)
 }
