d9fca7009323a57a36e642cfadeccb2cfddcac6b
Paul Calotta <paul.calotta@gmail.com>
2020-05-10 12:27:13 -0400

    Add Python solution for 7-kyu/selective-fear-of-numbers

---
+++ 7-kyu/selective-fear-of-numbers/python/solution.py
@@ -0,0 +1,12 @@
+lookup_table = {
+    'Monday'    : lambda n: n == 12,
+    'Tuesday'   : lambda n: n > 95,
+    'Wednesday' : lambda n: n == 34,
+    'Thursday'  : lambda n: n == 0,
+    'Friday'    : lambda n: n % 2 == 0,
+    'Saturday'  : lambda n: n == 56,
+    'Sunday'    : lambda n: abs(n) == 666
+}
+
+def am_I_afraid(day, num):
+    return lookup_table[day](num)
