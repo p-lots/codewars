85f201b6a4e20d65b873f115a9e7dcf11ddcbd5d
Paul Calotta <paul.calotta@gmail.com>
2020-07-21 00:39:36 -0400

    Fixed Python solution for 6-kyu/are-they-the--same--

--- 6-kyu/are-they-the--same--/python/solution.py
+++ 6-kyu/are-they-the--same--/python/solution.py
@@ -1,3 +1,2 @@
-func comp(_ a: [Int], _ b: [Int]) -> Bool {
-    return a.map { $0 * $0 }.sorted() == b.sorted()
-}
+def comp(array1, array2):
+    return sorted([n * n for n in array1]) == sorted(array2) if array1 != None and array2 != None else False
