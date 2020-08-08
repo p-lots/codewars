bc7cd2dbce1307f1c999190a78b65ab33453e4d1
Paul Calotta <paul.calotta@gmail.com>
2020-08-08 16:37:03 -0400

    Add Python solution for 8-kyu/grasshopper---create-the-rooms

---
+++ 8-kyu/grasshopper---create-the-rooms/python/solution.py
@@ -0,0 +1,4 @@
+rooms = {'room1': {'name': None, 'description': None, 'completed': None},
+    'room2': {'name': None, 'description': None, 'completed': None},
+    'room3': {'name': None, 'description': None, 'completed': None},
+}
