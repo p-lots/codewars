26eb14d4b4e9110982b07dc279e2b317f0b8054c
Paul Calotta <paul.calotta@gmail.com>
2020-08-19 01:12:35 -0400

    Updated solution for 5-kyu/weight-for-weight to Swift 5

--- 5-kyu/weight-for-weight/swift/solution.swift
+++ 5-kyu/weight-for-weight/swift/solution.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 func makeDigitSum(_ n: Int) -> Int {
-    let digits = String(n).characters.map { Int(String($0)) ?? 0 }
+    let digits = String(n).compactMap { Int(String($0)) }
     return digits.reduce(0, +)
 }
 
@@ -15,9 +15,9 @@ func sortWeights(_ lhs: Int, _ rhs: Int) -> Bool {
 
 func orderWeight(_ s: String) -> String {
     let sTrimmed = s.trimmingCharacters(in: .whitespaces)
-    guard !sTrimmed.isEmpty else { return String() }
+    guard !sTrimmed.isEmpty else { return "" }
     let sArr = sTrimmed.components(separatedBy: " ")
         .filter { !$0.isEmpty }
-        .map { $0.characters.map { Int(String($0)) ?? 0 }.reduce(0) { total, n in total * 10 + n } }
+        .map { $0.compactMap { Int(String($0)) }.reduce(0) { total, n in total * 10 + n } }
     return sArr.sorted(by: sortWeights).map { String($0) }.joined(separator: " ")
 }
