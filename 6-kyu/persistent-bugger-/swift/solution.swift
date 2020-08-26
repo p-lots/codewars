7bc5d7461e0d1276ec1f0247a9615cc83f53d89a
Paul Calotta <paul.calotta@gmail.com>
2020-08-26 13:37:49 -0400

    Updated Swift solution for 6-kyu/persistent-bugger to Swift 5

--- 6-kyu/persistent-bugger-/swift/solution.swift
+++ 6-kyu/persistent-bugger-/swift/solution.swift
@@ -1,9 +1,9 @@
 func persistence(for num: Int) -> Int {
     var numStr = String(num)
-    guard numStr.characters.count > 1 else { return 0 }
+    guard numStr.count > 1 else { return 0 }
     var ret = 0
-    while numStr.characters.count > 1 {
-        let next = numStr.characters.map { Int(String($0))! }.reduce(1, *)
+    while numStr.count > 1 {
+        let next = numStr.compactMap { Int(String($0)) }.reduce(1, *)
         numStr = String(next)
         ret += 1
     }
