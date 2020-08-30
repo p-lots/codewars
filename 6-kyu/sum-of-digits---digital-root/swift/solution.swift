1d2d5672545abc953f300c3594c87368630be9e6
Paul Calotta <paul.calotta@gmail.com>
2020-08-30 15:43:13 -0400

    Update Swift solution for 6-kyu/sum-of-digits---digital-root to Swift 5

--- 6-kyu/sum-of-digits---digital-root/swift/solution.swift
+++ 6-kyu/sum-of-digits---digital-root/swift/solution.swift
@@ -1,4 +1,4 @@
 func digitalRoot(of number: Int) -> Int {
-    let n = String(number).map { Int(String($0)) ?? 0 }.reduce(0, +)
+    let n = String(number).compactMap { Int(String($0)) }.reduce(0, +)
     return n > 10 ? digitalRoot(of: n) : n
 }
