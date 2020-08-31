b594e5e1cdb656703e95b816fb7d795bb8db2255
Paul Calotta <paul.calotta@gmail.com>
2020-08-31 15:44:42 -0400

    Fix Swift solution for 6-kyu/write-number-in-expanded-form

--- 6-kyu/write-number-in-expanded-form/swift/solution.swift
+++ 6-kyu/write-number-in-expanded-form/swift/solution.swift
@@ -1,5 +1,7 @@
 func expandedForm(_ num: Int) -> String {
-    let numArr = String(num).characters.map { String($0) }
+    let numArr = String(num).map { String($0) }
     var zeroesCount = numArr.count
-    return numArr.map { zeroesCount -= 1; return $0 == "0" ? "" : $0 + String(repeating: "0", count: zeroesCount) }.filter { $0 != "" }.joined(separator: " + ")
+    return numArr.map { zeroesCount -= 1; return $0 == "0" ? "" : $0 + String(repeating: "0", count: zeroesCount) }
+        .filter { $0 != "" }
+        .joined(separator: " + ")
 }
