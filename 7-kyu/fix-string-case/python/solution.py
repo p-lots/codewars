d2af46b7577cd5625454177024d8597219b75a97
Paul Calotta <paul.calotta@gmail.com>
2020-09-05 15:21:03 -0400

    Fix Python solution for 7-kyu/fix-string-case

--- 7-kyu/fix-string-case/python/solution.py
+++ 7-kyu/fix-string-case/python/solution.py
@@ -1,27 +1,4 @@
-#include <algorithm>
-#include <cctype>
-#include <string>
-#include <utility>
-
-std::pair<int, int> count_cases(const std::string& str)
-{
-    int lower = 0, upper = 0;
-    for (const char &c : str) {
-        if (std::isupper(c)) upper++;
-        else if (std::islower(c)) lower++;
-    }
-    return { upper, lower };
-}
-
-std::string solve(const std::string& str)
-{
-    auto case_counts = count_cases(str);
-    auto str_cpy = str;
-    if (case_counts.first > case_counts.second) { // upper > lower
-        std::transform(str_cpy.begin(), str_cpy.end(), str_cpy.begin(), ::toupper);
-    }
-    else { // upper < lower || upper == lower
-        std::transform(str_cpy.begin(), str_cpy.end(), str_cpy.begin(), ::tolower);
-    }
-    return str_cpy;
-}
+def solve(s):
+    upper_count = sum(1 for ch in s if ch.isupper())
+    lower_count = sum(1 for ch in s if ch.islower())
+    return s.upper() if upper_count > lower_count else s.lower()
