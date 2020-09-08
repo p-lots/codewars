5016724a53805fb68ecb4e213d42d28149b9e1b9
Paul Calotta <paul.calotta@gmail.com>
2020-09-08 19:25:05 -0400

    Fix Python solution for 7-kyu/moves-in-squared-strings-(i)

--- 7-kyu/moves-in-squared-strings-(i)/python/solution.py
+++ 7-kyu/moves-in-squared-strings-(i)/python/solution.py
@@ -1,10 +1,6 @@
-func horMirror(_ s: String) -> String {
-    return s.components(separatedBy: "\n").reversed().joined(separator: "\n")
-}
-func vertMirror(_ s: String) -> String {
-    return s.components(separatedBy: "\n").map { String($0.characters.reversed()) }.joined(separator: "\n")
-}
-// replace the dots with function parameter
-func oper(_ f: (String) -> String, _ s: String) -> String {
-    return f(s)
-} 
+def vert_mirror(strng):
+    return '\n'.join(line[::-1] for line in strng.split())
+def hor_mirror(strng):
+    return '\n'.join(line for line in reversed(strng.split()))
+def oper(fct, s):
+    return fct(s)
