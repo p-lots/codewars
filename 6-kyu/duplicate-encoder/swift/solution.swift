4451266cadfa0448752bab9be65c0ea9b7fdd188
Paul Calotta <paul.calotta@gmail.com>
2020-09-22 22:06:54 -0400

    Fix Swift solution for 6-kyu/duplicate-encoder

--- 6-kyu/duplicate-encoder/swift/solution.swift
+++ 6-kyu/duplicate-encoder/swift/solution.swift
@@ -1,9 +1,13 @@
-def duplicate_encode(word):
-    ret = ''
-    word = word.lower()
-    for ch in word:
-        if word.count(ch) == 1:
-            ret += '('
-        else:
-            ret += ')'
-    return ret
+extension String {
+    func countChar(_ charToCount: Character) -> Int {
+        return self.reduce(0) { (acc: Int, next: Character) -> Int in
+            return acc + ((next == charToCount) ? 1 : 0)
+        }
+    }
+}
+
+func duplicateEncode(_ word: String) -> String {
+    return word.lowercased().reduce("") { (acc: String, next: Character) -> String in
+        return acc + ((word.lowercased().countChar(next) == 1) ? "(" : ")")
+    }
+}
