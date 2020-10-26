42d9f4e7094ad98c8112a47e65d1533747039b39
Paul Calotta <paul.calotta@gmail.com>
2020-10-26 15:49:45 -0400

    Add C++ solution for 7-kyu/recursion-101

---
+++ 7-kyu/recursion-101/c++/solution.cpp
@@ -0,0 +1,11 @@
+std::pair<int, int> solve (int a, int b)
+{
+    if (a == 0 || b == 0) {
+        return { a, b };
+    } else if (a >= 2 * b) {
+        return solve(a - 2 * b, b);
+    } else if (b >= 2 * a) {
+        return solve(a, b - 2 * a);
+    }
+    return { a, b };
+}
