e309a8242474c61159bdbb854be8c72582a948b6
Paul Calotta <paul.calotta@gmail.com>
2021-06-11 12:30:50 -0400

    Fix Python solution for 7-kyu/disemvowel-trolls

--- 7-kyu/disemvowel-trolls/python/solution.py
+++ 7-kyu/disemvowel-trolls/python/solution.py
@@ -1,9 +1,2 @@
-func disemvowel(_ s: String) -> String {
-    var strArray = Array(s.characters)
-    strArray = strArray.filter({ !isVowel($0) })
-    return String(strArray)
-}
-
-func isVowel(_ s: Character) -> Bool {
-    return s == "a" || s == "e" || s == "i" || s == "o" || s == "u" || s == "A" || s == "E" || s == "I" || s == "O" || s == "U"
-}
+def disemvowel(strng):
+    return ''.join(ch for ch in strng if ch.lower() not in 'aeiou')
