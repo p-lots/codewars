a08a4fbdbdc524d27c37264d521ccd9f5847af57
Paul Calotta <paul.calotta@gmail.com>
2021-07-13 12:54:28 -0400

    Add Python solution for 7-kyu/check-for-prime-numbers

---
+++ 7-kyu/check-for-prime-numbers/python/solution.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
