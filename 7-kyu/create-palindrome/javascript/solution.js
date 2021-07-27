8dd6c3f1f50b0c39916f304d70482d6f250ffeaa
Paul Calotta <paul.calotta@gmail.com>
2021-07-27 12:41:26 -0400

    Add JavaScript solution for 7-kyu/create-palindrome

---
+++ 7-kyu/create-palindrome/javascript/solution.js
@@ -0,0 +1,4 @@
+const _ = require('lodash');
+function solve(s){
+    return (_.zip(s.split(""), s.split("").reverse()).map(e=>(e[0].charCodeAt(0)) - (e[1].charCodeAt(0)))).every(e=>[-2, 0, 2].includes(e));
+}
