0ef38274e836dcc0fa09a2033a5e99a032c7f3b8
Paul Calotta <paul.calotta@gmail.com>
2022-02-12 17:41:43 -0500

    Add JavaScript solution for 8-kyu/drink-about

---
+++ 8-kyu/drink-about/javascript/solution.js
@@ -0,0 +1,10 @@
+const peopleWithAgeDrink = old => {
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old < 18) {
+    return "drink coke";
+  } else if (old < 21) {
+    return "drink beer";
+  }
+  return "drink whisky";
+};
