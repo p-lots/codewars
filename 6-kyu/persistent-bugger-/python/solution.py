7126110f42b5dd46ea09705837d932eece094567
Paul Calotta <paul.calotta@gmail.com>
2023-04-01 15:52:31 -0400

    Update some 6-kyu solutions to fix mismatches

--- 6-kyu/persistent-bugger-/python/solution.py
+++ 6-kyu/persistent-bugger-/python/solution.py
@@ -1,16 +1,11 @@
-func persistence(for num: Int) -> Int {
-   var ret = 0
-   var num_str = String(num)
-   while num_str.characters.count > 1 {
-       var sum = 1
-       for c in num_str.characters {
-           let temp = String(c)
-           if let temp_int = Int(temp) {
-               sum *= temp_int
-           }
-       }
-       ret += 1
-       num_str = String(sum)
-   }
-   return ret
-}
+from functools import reduce
+from operator import mul
+
+def persistence(n):
+    if n < 10:
+        return 0
+    ret = 0
+    while len(str(n)) > 1:
+        n = reduce(mul, map(int, str(n)))
+        ret += 1
+    return ret
