bbf9e4690b67a1f04de8545cebac2db2437e1b30
Paul Calotta <paul.calotta@gmail.com>
2023-04-17 11:53:38 -0400

    Add JavaScript solution for 8-kyu/did-she-say-hallo

---
+++ 8-kyu/did-she-say-hallo-/javascript/solution.js
@@ -0,0 +1,6 @@
+import re
+
+def validate_hello(greetings):
+    greetings = [''.join(ch for ch in word if ch.isalpha()) for word in greetings.lower().split()]
+    hellos = ['hello', 'ciao', 'salut', 'hallo', 'hola', 'ahoj', 'czesc']
+    return any(hello in greetings for hello in hellos)
