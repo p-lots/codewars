78a40cd431e25132c5bd7cbaa3a0762c12576284
Paul Calotta <paul.calotta@gmail.com>
2023-07-27 14:09:21 -0400

    Add JavaScript solution for 7-kyu/coding-3min--bug-in-apple

---
+++ 7-kyu/coding-3min--bug-in-apple/javascript/solution.js
@@ -0,0 +1,10 @@
+const sc = (apple) => {
+  for (let i = 0; i < apple.length; i++) {
+    for (let j = 0; j < apple[i].length; j++) {
+      if (apple[i][j] === "B") {
+        return [i, j];
+      }
+    }
+  }
+  return [-1, -1];
+};
