6d113dfe113588158adea3c60bf663690aa4262f
Paul Calotta <paul.calotta@gmail.com>
2024-01-19 17:23:41 -0500

    Fix Python solution for 6-kyu/dictionary-merge

--- 6-kyu/dictionary-merge/python/solution.py
+++ 6-kyu/dictionary-merge/python/solution.py
@@ -1,18 +1,8 @@
-func merge<Key, Value>(_ dicts: [[Key: Value]]) -> [Key: [Value]] {
-  var ret = [Key: [Value]]()
-  for dict in dicts {
-    for (key, val) in dict {
-      var valsArr = [Value]()
-      if let valArrayFromRet = ret[key] {
-        valsArr = valArrayFromRet
-        valsArr.append(val)
-        ret.updateValue(valsArr, forKey: key)
-      }
-      else {
-        valsArr.append(val)
-        ret.updateValue(valsArr, forKey: key)
-      }
-    }
-  }
-  return ret
-}
+from collections import defaultdict
+
+def merge(*dicts):
+    ret = defaultdict(list)
+    for dct in dicts:
+        for key, val in dct.items():
+            ret[key].append(val)
+    return ret
