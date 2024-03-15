fd5b6e9155ec0c8f89fc228a954dc7483ccbc19e
Paul Calotta <15675751+p-lots@users.noreply.github.com>
2024-03-15 12:58:54 -0400

    Add TypeScript solution for 7-kyu/head,-tail,-init-and-last

---
+++ 7-kyu/head,-tail,-init-and-last/typescript/solution.ts
@@ -0,0 +1,7 @@
+export const head = (arr: number[]) => arr[0];
+
+export const tail = (arr: number[]) => arr.slice(1);
+
+export const init = (arr: number[]) => arr.slice(0, -1);
+
+export const last = (arr: number[]) => arr[arr.length - 1];
