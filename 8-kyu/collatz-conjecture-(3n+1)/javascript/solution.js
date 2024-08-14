aff33a0facda3bf40132b499250cee69363a0a2c
Paul Calotta <15675751+p-lots@users.noreply.github.com>
2024-08-14 12:20:46 -0400

    Add JavaScript solution for 8-kyu/collatz-conjecture-(3n+1)

---
+++ 8-kyu/collatz-conjecture-(3n+1)/javascript/solution.js
@@ -0,0 +1,12 @@
+const hotpo = n => {
+  let steps = 0;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n = Math.floor(n / 2);
+    } else {
+      n = 3 * n + 1;
+    }
+    steps++;
+  }
+  return steps;
+};
