ce910b7931ad6efce2b518b8460ac719550f3f32
Paul Calotta <15675751+p-lots@users.noreply.github.com>
2024-09-28 12:14:02 -0400

    Add JavaScript solution for 8-kyu/holiday-vi---shark-pontoon

---
+++ 8-kyu/holiday-vi---shark-pontoon/javascript/solution.js
@@ -0,0 +1,6 @@
+const shark = (pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) => {
+  if (dolphin) {
+    sharkSpeed /= 2;
+  }
+  return pontoonDistance / youSpeed < sharkDistance / sharkSpeed ? "Alive!" : "Shark Bait!";
+};
