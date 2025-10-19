ff545cad0a3a22ce934d09963a3d7f3e12e29173
Paul C <15675751+p-lots@users.noreply.github.com>
2025-10-19 10:33:41 -0400

    Add JavaScript solution for 8-kyu/quadrants

---
+++ 8-kyu/quadrants/javascript/solution.js
@@ -0,0 +1,10 @@
+const quadrant = (x, y) => {
+  if (x > 0 && y > 0) {
+    return 1;
+  } else if (x < 0 && y > 0) {
+    return 2;
+  } else if (x < 0 && y < 0) {
+    return 3;
+  }
+  return 4;
+}
