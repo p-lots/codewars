c484851ec4ad4178d29f54e4c9848fb5f0fa94ba
Paul C <15675751+p-lots@users.noreply.github.com>
2025-10-30 10:53:04 -0400

    Add JavaScript solution for 8-kyu/shifty-closures

---
+++ 8-kyu/shifty-closures/javascript/README.md
@@ -0,0 +1,3 @@
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greetAbe() actually greet Abe?
---
+++ 8-kyu/shifty-closures/javascript/solution.js
@@ -0,0 +1,7 @@
+let name = 'Abe';
+
+const greetAbe = (name = 'Abe') => 'Hello, ' + name + '!';
+
+name = 'Ben';
+
+const greetBen = (name = 'Ben') => 'Hello, ' + name + '!';
